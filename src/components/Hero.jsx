2081ccd101082be274b6c72c24c81f1a2220cde4
Gduffusug <120303361+Gduffusug@users.noreply.github.com>
2026-06-25 16:51:29 +0530

    Create Hero.jsx

---
+++ src/components/Hero.jsx
@@ -0,0 +1,22 @@
+export default function Hero(){
+
+  return (
+    <section className="hero">
+
+      <h1>
+        Build Faster With
+        <span> Zentra AI</span>
+      </h1>
+
+      <p>
+        Your all-in-one AI workspace for creators,
+        students and businesses.
+      </p>
+
+      <button>
+        Start Creating
+      </button>
+
+    </section>
+  )
+}
