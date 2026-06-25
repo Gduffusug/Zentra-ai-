b6225d0ea1059e9b49c306cf96fc05ab9a426df6
Gduffusug <120303361+Gduffusug@users.noreply.github.com>
2026-06-25 16:50:39 +0530

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+export default function Navbar(){
+
+  return (
+    <nav className="navbar">
+
+      <h2>Zentra</h2>
+
+      <div className="links">
+        <span>Tools</span>
+        <span>Pricing</span>
+        <span>Login</span>
+      </div>
+
+    </nav>
+  )
+}
