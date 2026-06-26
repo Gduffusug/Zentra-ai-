d67a2bf5c0d995420881283e45a4633bc626d773
Gduffusug <120303361+Gduffusug@users.noreply.github.com>
2026-06-27 00:06:58 +0530

    Update Chat.jsx

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,20 +1,47 @@
+import { useState } from "react";
+
+export default function Chat(){
+
+const [message,setMessage]=useState("");
+const [reply,setReply]=useState("");
+
+async function sendMessage(){
+
+const res = await fetch("/api/chat",{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body:JSON.stringify({
+message:message
+})
+});
+
+const data = await res.json();
+
+setReply(data.reply);
+
+}
+
+
 return(
 <div className="chat-container">
 
 <h1>Zentra AI 🤖</h1>
 
 <div className="chat-box">
-<p>{reply}</p>
+{reply}
 </div>
 
 
 <div className="input-area">
 
 <input
-placeholder="Message Zentra AI..."
 value={message}
+placeholder="Message Zentra AI..."
 onChange={(e)=>setMessage(e.target.value)}
 />
+
 
 <button onClick={sendMessage}>
 ➤
@@ -24,3 +51,5 @@ onChange={(e)=>setMessage(e.target.value)}
 
 </div>
 )
+
+}
