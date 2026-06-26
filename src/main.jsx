9b03720d4c412164e5de296fe1f4ed7656768e6a
Gduffusug <120303361+Gduffusug@users.noreply.github.com>
2026-06-26 14:35:55 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,26 +6,16 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Tools from "./components/Tools";
 
-
 function App(){
 
 return(
-<div>
-
+<>
 <Navbar />
-
 <Hero />
-
 <Tools />
-
-</div>
+</>
 )
 
 }
 
-
-createRoot(
-document.getElementById("root")
-).render(
-<App />
-);
+createRoot(document.getElementById("root")).render(<App />);
