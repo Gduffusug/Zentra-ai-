399abb24de107df3807799652e843c2634138686
Gduffusug <120303361+Gduffusug@users.noreply.github.com>
2026-06-26 15:19:31 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,40 @@ import "./style.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Tools from "./components/Tools";
+import Chat from "./pages/Chat";
+
 
 function App(){
 
+const path = window.location.pathname;
+
+
+if(path === "/chat"){
+
+return <Chat />;
+
+}
+
+
 return(
-<>
+
+<div>
+
 <Navbar />
+
 <Hero />
+
 <Tools />
-</>
+
+</div>
+
 )
 
 }
 
-createRoot(document.getElementById("root")).render(<App />);
+
+createRoot(
+document.getElementById("root")
+).render(
+<App />
+);
