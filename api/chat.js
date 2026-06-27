bf04ce89a192e3cc6136f0e28865ad01b7134c61
Gduffusug <120303361+Gduffusug@users.noreply.github.com>
2026-06-27 23:52:34 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -4,50 +4,41 @@ try{
 
 const {message}=req.body;
 
-
 const response = await fetch(
-`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-lite:generateContent?key=${process.env.GEMINI_API_KEY}`,
+"https://api.groq.com/openai/v1/chat/completions",
 {
 method:"POST",
 headers:{
-"Content-Type":"application/json"
+"Content-Type":"application/json",
+"Authorization":`Bearer ${process.env.GROQ_API_KEY}`
 },
 body:JSON.stringify({
-contents:[
+model:"openai/gpt-oss-120b",
+messages:[
 {
-parts:[
-{
-text:message
-}
-]
+role:"user",
+content:message
 }
 ]
 })
 }
 );
 
-
 const data = await response.json();
 
 console.log(data);
 
-
-if(!data.candidates){
-return res.status(200).json({
-reply:"Gemini Error: "+JSON.stringify(data)
-});
-}
-
-
-return res.status(200).json({
-reply:data.candidates[0].content.parts[0].text
+res.status(200).json({
+reply:data.choices[0].message.content
 });
 
 
 }catch(error){
 
-return res.status(500).json({
-reply:error.message
+console.log(error);
+
+res.status(500).json({
+reply:"Groq Error: "+error.message
 });
 
 }
