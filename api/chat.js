aa33ad214ee6558190890bb73396e93db64ed0c1
Gduffusug <120303361+Gduffusug@users.noreply.github.com>
2026-06-27 14:26:42 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -43,8 +43,7 @@ export default async function handler(req, res) {
 
 
     res.status(200).json({
-      reply: data.candidates[0].content.parts[0].text
-    });
+      reply:data.candidates?.[0]?.content?.parts?.[0]?.text || "No response"
 
 
   } catch (error) {
