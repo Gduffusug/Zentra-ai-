80f72841c2e1e93ec1fb5a040e9cca67d9b7ce61
Gduffusug <120303361+Gduffusug@users.noreply.github.com>
2026-06-28 19:28:41 +0000

    Update Firebase login

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+
 import "./style.css";
 
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Tools from "./components/Tools";
 import Chat from "./pages/Chat";
+import Login from "./components/Login";
 
 
 function App(){
@@ -14,31 +16,69 @@ const path = window.location.pathname;
 
 
 if(path === "/chat"){
+  return <Chat />;
+  }
 
-return <Chat />;
 
+  return(
+    <div>
+
+        <Login />
+
+            <Navbar />
+
+                <Hero />
+
+                    <Tools />
+
+                      </div>
+                      )
+
+                      }
+
+
+                      createRoot(
+                       document.getElementById("root")
+                       ).render(
+                        <App />
+                        );import React from "react";
+import { createRoot } from "react-dom/client";
+
+import "./style.css";
+
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Tools from "./components/Tools";
+import Chat from "./pages/Chat";
+import Login from "./components/Login";
+
+
+function App(){
+
+const path = window.location.pathname;
+
+
+if(path === "/chat"){
+  return <Chat />;
 }
 
 
 return(
+  <div>
 
-<div>
+    <Login />
 
-<Navbar />
+    <Navbar />
 
-<Hero />
+    <Hero />
 
-<Tools />
+    <Tools />
 
-</div>
-
+  </div>
 )
 
 }
 
 
 createRoot(
-document.getElementById("root")
-).render(
-<App />
-);
+ document.getElementById("ro
