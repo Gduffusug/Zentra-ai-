fa75d399a934a3193414eabba0c6204ae8f276af
Gduffusug <120303361+Gduffusug@users.noreply.github.com>
2026-06-29 00:20:52 +0530

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCSUfc9FAfxhbOloYr3m2Vr60NbcBkpaes",
+  authDomain: "zentra-ai-a9bdd.firebaseapp.com",
+  projectId: "zentra-ai-a9bdd",
+  storageBucket: "zentra-ai-a9bdd.firebasestorage.app",
+  messagingSenderId: "381054970940",
+  appId: "1:381054970940:web:b54dc23e2275d9fdc0e739",
+  measurementId: "G-XMHMEWLVWV"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
