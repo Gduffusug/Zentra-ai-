b92cf064709e8d17b55e56464bdcb254940b467f
Gduffusug <120303361+Gduffusug@users.noreply.github.com>
2026-06-29 01:11:08 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,84 +1,16 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 import "./style.css";
 
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Tools from "./components/Tools";
-import Chat from "./pages/Chat";
-import Login from "./components/Login";
-
-
-function App(){
-
-const path = window.location.pathname;
-
-
-if(path === "/chat"){
-  return <Chat />;
-  }
-
-
-  return(
-    <div>
-
-        <Login />
-
-            <Navbar />
-
-                <Hero />
-
-                    <Tools />
-
-                      </div>
-                      )
-
-                      }
-
-
-                      createRoot(
-                       document.getElementById("root")
-                       ).render(
-                        <App />
-                        );import React from "react";
-import { createRoot } from "react-dom/client";
-
-import "./style.css";
-
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Tools from "./components/Tools";
-import Chat from "./pages/Chat";
-import Login from "./components/Login";
-
-
-function App(){
-
-const path = window.location.pathname;
-
-
-if(path === "/chat"){
-  return <Chat />;
+function Root() {
+  return (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 }
 
-
-return(
-  <div>
-
-    <Login />
-
-    <Navbar />
-
-    <Hero />
-
-    <Tools />
-
-  </div>
-)
-
-}
-
-
-createRoot(
- document.getElementById("ro
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Root />
+);
