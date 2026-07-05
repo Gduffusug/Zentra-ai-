745184515e826e20f02f29de937345b7c4e99255
Gduffusug <120303361+Gduffusug@users.noreply.github.com>
2026-07-05 13:44:52 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from "./App";
 import Chat from "./pages/Chat";
-import MergePdf from "./pages/MergePdf";
+import MergePdf from "./pages/pdf/MergePdf";
 
-ReactDOM.createRoot(
-  document.getElementById("root")
-).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
-
       <Route path="/" element={<App />} />
-
       <Route path="/chat" element={<Chat />} />
-
-      <Route
-        path="/merge-pdf"
-        element={<MergePdf />}
-      />
-
+      <Route path="/merge-pdf" element={<MergePdf />} />
     </Routes>
   </BrowserRouter>
 );
