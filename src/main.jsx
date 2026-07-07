fb45df4d65708758625dfc2c7682b96c4027bff7
Gduffusug <120303361+Gduffusug@users.noreply.github.com>
2026-07-07 13:25:56 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,25 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Chat from "./pages/Chat";
 import MergePdf from "./pages/pdf/MergePdf";
+import AgeCalculator from "./pages/AgeCalculator";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/chat" element={<Chat />} />
-      <Route path="/merge-pdf" element={<MergePdf />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<App />} />
+
+        <Route path="/chat" element={<Chat />} />
+
+        <Route path="/merge-pdf" element={<MergePdf />} />
+
+        <Route
+          path="/age-calculator"
+          element={<AgeCalculator />}
+        />
+
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
