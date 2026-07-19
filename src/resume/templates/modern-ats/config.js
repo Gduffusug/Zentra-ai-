842a83028fdf23dc160724d11d67d221615d304f
Gduffusug <120303361+Gduffusug@users.noreply.github.com>
2026-07-19 12:30:52 +0530

    Create config.js

---
+++ src/resume/templates/modern-ats/config.js
@@ -0,0 +1,11 @@
+import thumbnail from "./thumbnail.png";
+
+const template = {
+  id: "modern-ats",
+  name: "Modern ATS",
+  type: "Free",
+  profession: "All Professions",
+  thumbnail,
+};
+
+export default template;
