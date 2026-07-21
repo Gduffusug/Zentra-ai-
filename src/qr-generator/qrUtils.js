005aa906d09b7d7d63c2fbba4916b6796284a509
Gduffusug <120303361+Gduffusug@users.noreply.github.com>
2026-07-21 14:49:48 +0530

    Create qrUtils.js

---
+++ src/qr-generator/qrUtils.js
@@ -0,0 +1,27 @@
+export const qrTypes = [
+  {
+    id: "url",
+    title: "Website",
+    placeholder: "https://example.com"
+  },
+  {
+    id: "text",
+    title: "Text",
+    placeholder: "Enter any text"
+  },
+  {
+    id: "email",
+    title: "Email",
+    placeholder: "example@gmail.com"
+  },
+  {
+    id: "phone",
+    title: "Phone",
+    placeholder: "+91 9876543***"
+  },
+  {
+    id: "wifi",
+    title: "WiFi",
+    placeholder: "SSID;PASSWORD"
+  }
+];
